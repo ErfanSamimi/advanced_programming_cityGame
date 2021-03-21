3837a5c3e10702f1ad2190cac2880cc5b017b5b7
Erfan Samimi <erf80sami@gmail.com>
2021-03-21 19:38:30 +0330

    add properties to Person class

--- Main/Person.java
+++ Main/Person.java
@@ -1,4 +1,12 @@
 package Main;
 
 public class Person {
+
+    String firstname ;
+    String lastname ;
+    int birthDate ;
+    String birthPlace ;
+    String job ;
+    String gender ;
+    int salay ;
 }
