7358495ad48821e218f070de5b217938beab7c4d
Erfan Samimi <erf80sami@gmail.com>
2021-03-21 20:01:19 +0330

    add properties and constructor to Vehicle class

--- Main/Vehicles/Vehicle.java
+++ Main/Vehicles/Vehicle.java
@@ -1,4 +1,28 @@
 package Main.Vehicles;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Vehicle {
+    int price ;
+    int capacity ;
+    String ID ;
+    String name_builder_company ;
+    private static ArrayList<String> ID_list = new ArrayList<String>();
+    //==================================================
+
+    Vehicle(int price , int capacity , String ID , String name_builder_company){
+        this.price = price ;
+        this.capacity = capacity ;
+        this.name_builder_company = name_builder_company ;
+        if(ID_list.contains(ID)){
+            System.out.println("This ID exists ! Please Set new ID for this vehicle .");
+            this.ID ="00000000";
+        }
+        else
+            this.ID =ID;
+
+    }
+
+
 }
