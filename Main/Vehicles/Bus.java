ea9dfa1baeae979a2ce35e64fa65d7d67cf4152e
Erfan Samimi <erf80sami@gmail.com>
2021-03-21 21:51:46 +0330

    add properties and constructor in Bus class

--- Main/Vehicles/Bus.java
+++ Main/Vehicles/Bus.java
@@ -1,4 +1,12 @@
 package Main.Vehicles;
 
-public class Bus {
+public class Bus extends Land_transport_vehicle{
+    String class_type ; //business class , economy class
+    int KPL ;  //kilometer per liter
+
+    Bus(String class_type , int KPL , int fuel_capacity , String fuel_type , int speed , int price , int passenger_capacity , String name_company , String ID){
+        super(fuel_capacity , fuel_type , speed , price , passenger_capacity , name_company , ID);
+        this.class_type = class_type;
+        this.KPL = KPL ;
+    }
 }
