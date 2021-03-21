3ab7f6c568b4995957c97fc01cee7f50978a2881
Erfan Samimi <erf80sami@gmail.com>
2021-03-21 20:45:48 +0330

    add properties and constructor to Air_transport_vehicle

--- Main/Vehicles/Air_transport_vehicle.java
+++ Main/Vehicles/Air_transport_vehicle.java
@@ -1,4 +1,12 @@
 package Main.Vehicles;
 
-public class Air_transport_vehicle {
+abstract public class Air_transport_vehicle extends Vehicle {
+    int max_flight_height ;
+    int runway_length ;
+
+    Air_transport_vehicle(int max_flight_height , int runway_length , int price , int capacity , String ID , String companyName ){
+        super(price , capacity , ID , companyName) ;
+        this.max_flight_height = max_flight_height ;
+        this.runway_length = runway_length;
+    }
 }
