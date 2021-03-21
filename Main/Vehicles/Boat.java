b0ffd558a660dc80949adde19f840d4c95e2b634
Erfan Samimi <erf80sami@gmail.com>
2021-03-21 22:05:46 +0330

    add properties and constructor in Boat class

--- Main/Vehicles/Boat.java
+++ Main/Vehicles/Boat.java
@@ -1,4 +1,10 @@
 package Main.Vehicles;
 
-public class Boat {
+public class Boat extends Shipping_vehicle{
+    int max_distance ;
+
+    Boat(int max_distance , String fuel_type , int min_depth , int price , int capacity , String ID , String companyName){
+        super(fuel_type , min_depth ,price , capacity , ID , companyName);
+        this.max_distance = max_distance;
+    }
 }
