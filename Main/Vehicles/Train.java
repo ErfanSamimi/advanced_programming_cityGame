e514721b043a2e5763b8790b32e22ea4b19345cd
Erfan Samimi <erf80sami@gmail.com>
2021-03-21 21:42:03 +0330

    add properties and constructor and Services enum in Train class

--- Main/Vehicles/Train.java
+++ Main/Vehicles/Train.java
@@ -1,4 +1,19 @@
 package Main.Vehicles;
 
-public class Train {
+public class Train extends Land_transport_vehicle{
+
+    enum Services {
+        Restaurant , Breakfast , Lunch , Dinner
+    }
+
+    int number_of_wagons ;
+    int stars ;
+
+    Train(int number_of_wagons , int stars , int fuel_capacity , String fuel_type , int speed , int price , int passenger_capacity , String name_company , String ID ){
+        super(fuel_capacity , fuel_type ,speed ,price ,passenger_capacity ,name_company , ID);
+        this.number_of_wagons = number_of_wagons ;
+        this.stars = stars ;
+    }
 }
+
+
