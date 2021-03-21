9c0a3127385fc1cd09529e21e23a6353e18f1ffc
Erfan Samimi <erf80sami@gmail.com>
2021-03-21 21:59:06 +0330

    add properties and constructor in Ship class

--- Main/Vehicles/Ship.java
+++ Main/Vehicles/Ship.java
@@ -1,4 +1,12 @@
 package Main.Vehicles;
 
-public class Ship {
+public class Ship extends Shipping_vehicle {
+    String classType ;
+    int shipLength ;
+
+    Ship(String classType , int shipLength , String fuel_type , int min_depth , int price , int capacity , String ID , String companyName){
+        super(fuel_type , min_depth , price ,capacity ,ID ,companyName);
+        this.classType = classType ;
+        this.shipLength = shipLength;
+    }
 }
