138447767a49dc5543fea966ecc18b2ad9152785
Erfan Samimi <erf80sami@gmail.com>
2021-03-22 11:03:59 +0430

    add properties and constructor in ShippingPort class and getter/setter method for ShipList

--- Main/Building/ShippingPort.java
+++ Main/Building/ShippingPort.java
@@ -1,4 +1,26 @@
 package Main.Building;
 
-public class ShippingPort {
+import Main.Vehicles.Shipping_vehicle;
+
+import java.util.ArrayList;
+
+public class ShippingPort extends  Terminal {
+    private static ArrayList<Shipping_vehicle> shipList = new ArrayList<Shipping_vehicle>() ;
+    int number_of_waterfront ;
+
+    //==========================================
+     ShippingPort ( int number_of_waterfront , int build_price , String cityName , String terminalName , String address , int area , int number_of_vehicles){
+         super( build_price , cityName , terminalName , address , area , number_of_vehicles);
+         this.number_of_waterfront = number_of_waterfront ;
+     }
+
+     //==========================================
+
+    void setShipList( Shipping_vehicle vehicle){
+         shipList.add(vehicle);
+    }
+
+    ArrayList<Shipping_vehicle> getShipList () {
+         return shipList ;
+    }
 }
