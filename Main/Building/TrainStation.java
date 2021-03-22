a8b88db5c980fa33ce6037a7f87d3325398021a0
Erfan Samimi <erf80sami@gmail.com>
2021-03-22 10:44:23 +0430

    add properties and constructor in TrainStation class and make getter/setter method for TrainLis

--- Main/Building/TrainStation.java
+++ Main/Building/TrainStation.java
@@ -1,4 +1,30 @@
 package Main.Building;
 
-public class TrainStation {
+import Main.Vehicles.Train;
+
+import java.util.ArrayList;
+
+public class TrainStation extends Terminal{
+    private static ArrayList<Train> trainList = new ArrayList<Train>() ;
+    int inputRails ;
+    int outputRails ;
+
+    //===============================================================
+
+    TrainStation (int inputRails , int outputRails , int build_price , String cityName , String terminalName , String address , int area , int number_of_vehicles){
+        super(build_price , cityName , terminalName , address ,area ,number_of_vehicles);
+
+        this.inputRails = inputRails ;
+        this.outputRails = outputRails ;
+    }
+
+    //================================================================
+
+    void setTrainList(Train train){
+        trainList.add(train) ;
+    }
+
+    ArrayList<Train> getTrainList (){
+        return  trainList ;
+    }
 }
