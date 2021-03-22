d36f1ef4168ae344e7f59478f468c621c1801084
Erfan Samimi <erf80sami@gmail.com>
2021-03-22 10:19:24 +0430

    add properties and constructor in Terminal class and getter/setter method for EmployeesList

--- Main/Building/Terminal.java
+++ Main/Building/Terminal.java
@@ -1,4 +1,34 @@
 package Main.Building;
 
-public class Terminal {
+import Main.Person;
+
+import java.util.ArrayList;
+
+abstract public class Terminal {
+    int build_price ;
+    String cityName ;
+    String terminalName ;
+    String address ;
+    int area ;
+    int number_of_vehicles ;
+    private static ArrayList<Person> employees = new ArrayList<Person>();
+
+    //=============================================================================================
+
+    Terminal(int build_price , String cityName , String terminalName , String address , int area , int number_of_vehicles){
+        this.build_price = build_price ;
+        this.cityName = cityName ;
+        this.terminalName = terminalName ;
+        this.address = address ;
+        this.area = area ;
+        this.number_of_vehicles = number_of_vehicles ;
+    }
+    //==============================================================================================
+
+    void setEmployees(Person empl){
+        employees.add(empl);
+    }
+    ArrayList<Person> getEmployees(){
+        return employees ;
+    }
 }
