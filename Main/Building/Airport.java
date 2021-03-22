e6173f078ae7e50d64784a82454717c7e1b46903
Erfan Samimi <erf80sami@gmail.com>
2021-03-22 10:36:38 +0430

    add properties and constructor in Airport class and getter/setter method for AirplaneList

--- Main/Building/Airport.java
+++ Main/Building/Airport.java
@@ -1,4 +1,29 @@
 package Main.Building;
 
-public class Airport {
+import Main.Vehicles.Air_transport_vehicle;
+
+import java.util.ArrayList;
+
+public class Airport extends Terminal {
+    private static ArrayList <Air_transport_vehicle> airplaneList = new ArrayList <Air_transport_vehicle>();
+    boolean international_Airport ;
+    int number_of_runways ;
+     //====================================================================
+
+    Airport(boolean international_Airport , int number_of_runways , int build_price , String cityName , String terminalName , String address , int area , int number_of_vehicles){
+        super(build_price ,cityName ,terminalName ,address , area ,number_of_vehicles);
+
+        this.international_Airport = international_Airport ;
+        this.number_of_runways = number_of_runways ;
+    }
+
+    //======================================================================
+
+    void setAirplaneList(Air_transport_vehicle vehicle){
+        airplaneList.add(vehicle) ;
+    }
+
+    ArrayList <Air_transport_vehicle> getAirplaneList(){
+        return airplaneList;
+    }
 }
