a231fad98a3a75d116e59155c48c55c1b6f4fa8a
Erfan Samimi <erf80sami@gmail.com>
2021-03-22 10:26:56 +0430

    add properties and constructor in Bus_Terminal class and getter/setter method for busesList

--- Main/Building/Bus_Terminal.java
+++ Main/Building/Bus_Terminal.java
@@ -1,4 +1,26 @@
 package Main.Building;
 
-public class Bus_Terminal {
+import Main.Vehicles.Bus;
+
+import java.util.ArrayList;
+
+public class Bus_Terminal extends Terminal{
+    private static ArrayList<Bus> busesList = new ArrayList<Bus>();
+
+    //=======================================================================
+
+    Bus_Terminal(int build_price , String cityName , String terminalName , String address , int area , int number_of_vehicles){
+        super(build_price , cityName ,terminalName ,address , area ,number_of_vehicles);
+    }
+
+    //=======================================================================
+
+    void setBusesList(Bus bus){
+        busesList.add(bus);
+    }
+
+    ArrayList<Bus> getBusesList(){
+        return busesList ;
+    }
+
 }
