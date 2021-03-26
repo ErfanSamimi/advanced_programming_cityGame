1b6071b272528d475e3859e88bd7f7542d0ebf00
Erfan Samimi <erf80sami@gmail.com>
2021-03-26 12:43:07 +0430

    add price and edit constructor and make properties private

--- Main/Vehicles/Ship.java
+++ Main/Vehicles/Ship.java
@@ -1,11 +1,11 @@
 package Main.Vehicles;
 
 public class Ship extends Shipping_vehicle {
-    String classType ;
-    int shipLength ;
+    private String classType ;
+    private int shipLength ;
 
-    Ship(String classType , int shipLength , String fuel_type , int min_depth , int price , int capacity , String ID , String companyName){
-        super(fuel_type , min_depth , price ,capacity ,ID ,companyName);
+    Ship(String classType , int shipLength , String fuel_type , int min_depth , int capacity , String ID , String companyName){
+        super(fuel_type , min_depth , 600 ,capacity ,ID ,companyName);
         this.classType = classType ;
         this.shipLength = shipLength;
     }
