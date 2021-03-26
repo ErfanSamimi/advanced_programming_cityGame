4a8589832439f7fe393f268dcc445b78e7143dea
Erfan Samimi <erf80sami@gmail.com>
2021-03-26 12:39:51 +0430

    add price and edit constructor and make properties private

--- Main/Vehicles/Bus.java
+++ Main/Vehicles/Bus.java
@@ -1,11 +1,11 @@
 package Main.Vehicles;
 
 public class Bus extends Land_transport_vehicle{
-    String class_type ; //business class , economy class
-    int KPL ;  //kilometer per liter
+    private String class_type ; //business class , economy class
+    private int KPL ;  //kilometer per liter
 
-    Bus(String class_type , int KPL , int fuel_capacity , String fuel_type , int speed , int price , int passenger_capacity , String name_company , String ID){
-        super(fuel_capacity , fuel_type , speed , price , passenger_capacity , name_company , ID);
+    Bus(String class_type , int KPL , int fuel_capacity , String fuel_type , int speed , int passenger_capacity , String name_company , String ID){
+        super(fuel_capacity , fuel_type , speed , 200 , passenger_capacity , name_company , ID);
         this.class_type = class_type;
         this.KPL = KPL ;
     }
