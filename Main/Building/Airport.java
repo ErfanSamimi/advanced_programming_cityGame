a594b275f3d6372bfafd57c1661d2388c7d24e39
Erfan Samimi <erf80sami@gmail.com>
2021-03-26 13:22:02 +0430

    add number of workers and its getter method

--- Main/Building/Airport.java
+++ Main/Building/Airport.java
@@ -8,11 +8,12 @@ public class Airport extends Terminal {
     private static ArrayList <Air_transport_vehicle> airplaneList = new ArrayList <Air_transport_vehicle>();
     private boolean international_Airport ;
     private int number_of_runways ;
+    private int number_of_workers ;
      //====================================================================
 
-    Airport(boolean international_Airport , int number_of_runways ,  String cityName , String terminalName , String address , int area , int number_of_vehicles){
-        super(1400 ,cityName ,terminalName ,address , area ,number_of_vehicles);
-
+    Airport(boolean international_Airport , int number_of_runways ,  String cityName , String terminalName , String address , int area , int number_of_vehicles , int number_of_workers){
+        super(1400 + number_of_workers*30 ,cityName ,terminalName ,address , area ,number_of_vehicles);
+        this.number_of_workers = number_of_workers ;
         this.international_Airport = international_Airport ;
         this.number_of_runways = number_of_runways ;
     }
@@ -25,5 +26,9 @@ public class Airport extends Terminal {
 
     ArrayList <Air_transport_vehicle> getAirplaneList(){
         return airplaneList;
+    }
+
+    int getAirportPrice(){
+        return super.build_price ;
     }
 }
