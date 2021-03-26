737ee232def01edc5854a9e58fc550e33d629966
Erfan Samimi <erf80sami@gmail.com>
2021-03-26 12:42:21 +0430

    add price and edit constructor and make properties private

--- Main/Vehicles/Passenger_airplane.java
+++ Main/Vehicles/Passenger_airplane.java
@@ -1,13 +1,13 @@
 package Main.Vehicles;
 
 public class Passenger_airplane extends Air_transport_vehicle {
-    int number_of_workers ;
-    String classType ;     // business class , economy class
+    private int number_of_workers ;
+    private String classType ;     // business class , economy class
 
     Passenger_airplane(int number_of_workers , String classType , int max_flight_height , int runway_length
-                                                , int price , int capacity , String ID , String companyName){
+                                                , int capacity , String ID , String companyName){
 
-        super(max_flight_height ,runway_length ,price ,capacity ,ID ,companyName);
+        super(max_flight_height ,runway_length , 800 ,capacity ,ID ,companyName);
         this.number_of_workers = number_of_workers ;
         this.classType = classType;
 
