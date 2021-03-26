6c0c74ef1366a601467082eb54fe63275da57f38
Erfan Samimi <erf80sami@gmail.com>
2021-03-26 12:34:51 +0430

    add price and edit constructor and make properties private

--- Main/Building/ShippingPort.java
+++ Main/Building/ShippingPort.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 
 public class ShippingPort extends  Terminal {
     private static ArrayList<Shipping_vehicle> shipList = new ArrayList<Shipping_vehicle>() ;
-    int number_of_waterfront ;
+    private int number_of_waterfront ;
 
     //==========================================
-     ShippingPort ( int number_of_waterfront , int build_price , String cityName , String terminalName , String address , int area , int number_of_vehicles){
-         super( build_price , cityName , terminalName , address , area , number_of_vehicles);
+     ShippingPort ( int number_of_waterfront , String cityName , String terminalName , String address , int area , int number_of_vehicles){
+         super( 1000 , cityName , terminalName , address , area , number_of_vehicles);
          this.number_of_waterfront = number_of_waterfront ;
      }
 
