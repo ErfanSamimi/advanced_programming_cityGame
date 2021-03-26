ea0e2819e8b6d52f137e9db321db09fb88c82d94
Erfan Samimi <erf80sami@gmail.com>
2021-03-26 13:03:24 +0430

    change workers to stewardess

--- Main/Vehicles/Passenger_airplane.java
+++ Main/Vehicles/Passenger_airplane.java
@@ -1,14 +1,14 @@
 package Main.Vehicles;
 
 public class Passenger_airplane extends Air_transport_vehicle {
-    private int number_of_workers ;
+    private int number_of_stewardess ;
     private String classType ;     // business class , economy class
 
-    Passenger_airplane(int number_of_workers , String classType , int max_flight_height , int runway_length
+    Passenger_airplane(int number_of_stewardess , String classType , int max_flight_height , int runway_length
                                                 , int capacity , String ID , String companyName){
 
         super(max_flight_height ,runway_length , 800 ,capacity ,ID ,companyName);
-        this.number_of_workers = number_of_workers ;
+        this.number_of_stewardess = number_of_stewardess ;
         this.classType = classType;
 
     }
