d1bb481b418a6c864fb6acb75c649fa16ec67923
erfan <erf80sami@gmail.com>
2021-04-26 10:03:25 +0430

    make Safarable interface

---
+++ Main/Safar/Safarable.java
@@ -0,0 +1,17 @@
+package Main.Safar;
+
+import Main.Building.Terminal;
+import Main.Person;
+import Main.Vehicles.Vehicle;
+
+import java.util.ArrayList;
+
+public interface Safarable {
+
+    Safar newJourney(Terminal startingTerminal , Terminal destinationTerminal , ArrayList<Person> passengerList , Person driver , Vehicle vehicle , String journeyID , int journeyDay , int journeyMonth , int price);
+    void sortJourneys();
+    int journeyPrice ( ArrayList<Person> passengerList , Vehicle vehicle);
+
+    void journeyHistory();
+
+}
