8ab3730ad20160987dc84bcf448572c8e0c73b2d
erfan <erf80sami@gmail.com>
2021-06-05 16:47:09 +0430

    add save and restore in Passenger_airplane.java

--- Main/Vehicles/Passenger_airplane.java
+++ Main/Vehicles/Passenger_airplane.java
@@ -1,6 +1,11 @@
 package Main.Vehicles;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Passenger_airplane extends Air_transport_vehicle {
 
@@ -22,4 +27,45 @@ public class Passenger_airplane extends Air_transport_vehicle {
     public static ArrayList<Passenger_airplane> getPassenger_airplanesList(){
         return passenger_airplanesList;
     }
+
+
+
+    //======================================================================================= Saving Passenger airplanes
+
+
+    private static String address = "/home/erfan/Projects/Java/Files/CityGame/passenger_airplanes.csv" ;
+    static boolean append = false;
+
+
+
+    public void savePassenger_airplane() throws IOException {
+
+        FileOutputStream fout = new FileOutputStream(address , append);
+        PrintWriter pw = new PrintWriter(fout);
+
+        pw.format("%d,%s,%d,%d,%d,%s,%s\n" , this.number_of_flightAttendants , this.classType , this.max_flight_height , this.runway_length , this.getCapacity() , this.getID() , this.getName_builder_company()  );
+        append = true;
+        pw.close();
+        fout.close();
+    }
+
+    public static void  restorePassenger_airplane() throws IOException {
+
+        FileInputStream fin = new FileInputStream( address );
+        Scanner sc = new Scanner(fin);
+
+        while (sc.hasNextLine()) {
+
+            String[] airplane = sc.nextLine().split(",");
+
+
+            Passenger_airplane newAirplane = new Passenger_airplane( Integer.parseInt(airplane[0])  , airplane[1] ,  Integer.parseInt(airplane[2]) ,  Integer.parseInt(airplane[3]) ,  Integer.parseInt(airplane[4]) ,
+                    airplane[5] , airplane[6] );
+
+        }
+
+        fin.close();
+    }
 }
+
+
