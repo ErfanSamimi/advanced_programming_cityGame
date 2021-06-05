fe1e761cfbbff20ddf40420e9cf24a02cd867f03
erfan <erf80sami@gmail.com>
2021-06-05 16:44:25 +0430

    add save and restore in CargoPlane.java

--- Main/Vehicles/CargoPlane.java
+++ Main/Vehicles/CargoPlane.java
@@ -1,6 +1,11 @@
 package Main.Vehicles;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CargoPlane extends Air_transport_vehicle {
     private int total_weight ;
@@ -14,5 +19,38 @@ public class CargoPlane extends Air_transport_vehicle {
 
     public static ArrayList<CargoPlane> getCargoPlanesList(){
         return cargoPlanesList;
+    }
+
+    //======================================================================================= Saving cargo planes
+
+
+    private static String address = "/home/erfan/Projects/Java/Files/CityGame/cargoPlanes.csv" ;
+    static boolean append = false;
+
+    public void saveCargoPlane() throws IOException {
+
+        FileOutputStream fout = new FileOutputStream( address , append);
+        PrintWriter pw = new PrintWriter(fout);
+
+        pw.format("%d,%d,%d,%d,%s,%s\n" , this.total_weight , this.max_flight_height , this.runway_length ,this.getCapacity() , this.getID() ,this.getName_builder_company() );
+        append = true;
+        pw.close();
+        fout.close();
+    }
+
+    public static void restoreCargoPlane() throws IOException {
+
+        FileInputStream fin = new FileInputStream(address );
+        Scanner sc = new Scanner(fin);
+
+        while (sc.hasNextLine()) {
+
+            String[] cargoPlane = sc.nextLine().split(",");
+
+            CargoPlane newCargoPlane = new CargoPlane( Integer.parseInt(cargoPlane[0]) ,  Integer.parseInt(cargoPlane[1]) ,  Integer.parseInt(cargoPlane[2])  ,  Integer.parseInt(cargoPlane[3]) , cargoPlane[4] , cargoPlane[5] );
+
+        }
+
+        fin.close();
     }
 }
