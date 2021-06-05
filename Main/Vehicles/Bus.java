91b692a5f31191e76f4a53919c599ac6a7cc091f
erfan <erf80sami@gmail.com>
2021-06-05 16:40:33 +0430

    add save and restore in Bus.java

--- Main/Vehicles/Bus.java
+++ Main/Vehicles/Bus.java
@@ -1,6 +1,11 @@
 package Main.Vehicles;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bus extends Land_transport_vehicle{
     private String class_type ; //business class , economy class
@@ -17,4 +22,41 @@ public class Bus extends Land_transport_vehicle{
     public static ArrayList<Bus> getBusesList(){
         return busesList;
     }
+
+
+    // ========================================================================================== Saving bus
+
+    private static String address = "/home/erfan/Projects/Java/Files/CityGame/buses.csv" ;
+    static boolean append = false;
+
+    public void saveBus() throws IOException {
+
+        FileOutputStream fout = new FileOutputStream(address, append);
+        PrintWriter pw = new PrintWriter(fout);
+
+        pw.format("%s,%d,%d,%s,%d,%d,%s,%s\n" , this.class_type , this.KPL , this.fuel_capacity , this.class_type , this.speed , this.getCapacity() , this.getName_builder_company() , this.getID() );
+        append = true;
+        pw.close();
+        fout.close();
+    }
+
+
+
+    public static void restoreBuses() throws IOException {
+
+        FileInputStream fin = new FileInputStream(address);
+        Scanner sc = new Scanner(fin);
+
+        while (sc.hasNextLine()) {
+
+            String[] bus = sc.nextLine().split(",");
+
+            Bus newBus = new Bus(bus[0]  ,  Integer.parseInt(bus[1]) , Integer.parseInt(bus[2])  , bus[3]  ,  Integer.parseInt(bus[4]) , Integer.parseInt(bus[5]) , bus[6] , bus[7]  );
+
+        }
+
+        fin.close();
+    }
+
+
 }
