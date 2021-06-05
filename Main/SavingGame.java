26aa6bdb0050d57656ff3965c8ef3796cd37abeb
erfan <erf80sami@gmail.com>
2021-06-05 19:48:01 +0430

    add clearAllFiles() in SavingGame.java

--- Main/SavingGame.java
+++ Main/SavingGame.java
@@ -4,7 +4,9 @@ import Main.Building.*;
 import Main.Safar.Safar;
 import Main.Vehicles.*;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SavingGame {
 
@@ -42,4 +44,35 @@ public class SavingGame {
 
 
     }
+
+    static void clearAllFiles()   {
+        try {
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/people.csv");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/trainStations.txt");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/shippingPorts.txt");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/hotels.txt");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/cities.txt");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/busTerminals.txt");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/airports.txt");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/safarIDs.csv");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/trains.csv");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/ships.csv");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/buses.csv");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/boats.csv");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/cargoPlanes.csv");
+            clear_a_file("/home/erfan/Projects/Java/Files/CityGame/passenger_airplanes.csv");
+        }
+        catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    static void clear_a_file(String address) throws FileNotFoundException {
+
+        PrintWriter pw = new PrintWriter(address);
+        pw.close();
+
+    }
+
+
 }
