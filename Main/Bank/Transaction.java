83dfbcfe283a33cfa3025f3bc4a53be2ed7aec98
Erfan <erf80sami@gmail.com>
2021-06-25 11:50:00 +0430

    make Transaction.java

---
+++ Main/Bank/Transaction.java
@@ -0,0 +1,24 @@
+package Main.Bank;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Transaction {
+
+    int amount ;
+    String time ;
+    TransactionType type ;
+
+    public Transaction(int amount , TransactionType type ){
+        this.amount =  amount;
+        this.type = type;
+        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        this.time = dtf.format(now);
+    }
+
+
+}
+
+
