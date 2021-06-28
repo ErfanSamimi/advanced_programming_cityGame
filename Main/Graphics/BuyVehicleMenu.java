67162ebd8be067e8173f74df6a30d110b75fcd5a
Erfan <erf80sami@gmail.com>
2021-06-28 08:26:13 +0430

    add BuyShip.java GUI

--- Main/Graphics/BuyVehicleMenu.java
+++ Main/Graphics/BuyVehicleMenu.java
@@ -155,6 +155,8 @@ public class BuyVehicleMenu extends javax.swing.JFrame {
         // TODO add your handling code here:
 
         dispose();
+
+        new BuyShip();
     }
 
     private void trainButtonActionPerformed(java.awt.event.ActionEvent evt) {
